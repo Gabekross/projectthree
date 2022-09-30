3f2becf0bb22ac8f4263fa91d996a06fd1acbaa5
Gabekross <olugabriel80@gmail.com>
2022-09-29 23:28:15 -0700

    worked n JSON

--- flaskapi/alta3research_flask01.py
+++ flaskapi/alta3research_flask01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # NECCESARY IMPORTS
-from unicodedata import name
+
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -13,29 +13,69 @@ from flask import render_template
 app = Flask(__name__)
 
 # Create some test data for our catalog in the form of a list of dictionaries.
-books = [
+countries = [
     {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
+     'country': 'Liberia',
+     'capital': 'Monrovia',
+     'independence_date': 'July 26, 1847',
+     'prior_ruling_country': '', },
+
+     {'id': 1,
+     'country': 'South Africa',
+     'capital': 'Johannesbourg',
+      'independence_date': 'May 31, 1910',
+     'prior_ruling_country': 'Britain.',},
+
     {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
+        'country': 'Egypt',
+        'capital': '1992',
+        'independence_date': 'Feb 28, 1922',
+        'prior_ruling_country': 'Britain', },
+
+    {'id': 3,
+     'country': 'Ethiopia',
+     'capital': 'Addis Ababa',
+     'independence_date': 'May 5, 1941',
+     'prior_ruling_country': 'Italy', },
+
+    {'id': 4,
+        'country': 'Libya',
+        'capital': 'Tripoli',
+        'independence_date': 'Dec 24, 1951',
+        'prior_ruling_country': 'Britain', },
+
+
+    {'id': 5,
+     'country': 'Sudan',
+     'capital': 'Khartoum',
+     'independence_date': 'Jan 1, 1956',
+     'prior_ruling_country': 'Britain/Egypt', },
+
+    {'id': 6,
+        'country': 'Nigeria',
+        'capital': 'Abuja',
+        'independence_date': 'Oct 1, 1960',
+        'prior_ruling_country': 'Britain', },
+    {'id': 7,
+     'country': 'Ghana',
+     'capital': 'Accra',
+     'independence_date': '',
+     'prior_ruling_country': 'Britain', },
+
+    {'id': 8,
+         'country': 'Congo ',
+         'capital': 'Kinshasha',
+         'independence_date': 'June 30, 1960',
+         'prior_ruling_country': 'Britain', },
+
+  
 ]
 
 
 @app.route("/admin/")
 def home():
     return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+<p>A prototype API for discovering the continent of Africa!!!</p>'''
 
 
 @app.route("/<username>")
@@ -46,7 +86,7 @@ def index(username):
 @app.route("/")
 def glossary():
     # returns Books in JSON format
-    return jsonify(books)
+    return jsonify(countries)
 
 
 if __name__ == "__main__":
